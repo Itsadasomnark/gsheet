1d2f3521855e8f2b72f82d5f47978d627404bca5
Itsada <75968122+Itsadasomnark@users.noreply.github.com>
2020-12-14 15:04:46 +0700

    first commit

---
+++ googlesheet.py
@@ -0,0 +1,78 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+from pprint import pprint
+import pandas as pd
+################# install gspread ##################
+######## install or upgrade oauth2client ###########
+
+
+##############################------SCOPE------###################################
+'''	sheets readonly      = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+	sheets read/write    = 'https://www.googleapis.com/auth/spreadsheets'
+	drive readonly       = 'https://www.googleapis.com/auth/drive.readonly'
+	drive created/opened = 'https://www.googleapis.com/auth/drive.file'
+	drive full access    = 'https://www.googleapis.com/auth/drive' '''
+##################################################################################
+
+def createAccount(keysfile,scope):
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(keysfile,scope)
+	client = gspread.authorize(credentials)
+
+	return client
+
+def openByTitle(client,title):
+	sheet = client.open(title)
+
+	return sheet
+
+def openByURL(client,url):
+	sheet = client.open_by_url(url)
+
+	return sheet
+
+def createNewSheet(client,name):
+	sheet = client.create(name)
+
+	return sheet
+
+def shareSheet(sheet,email,account_type,role):
+	############## account_type #################
+	      #user,group,domain,anyone
+   	################### role ####################
+   			#owner, writer, reader
+	sheet.share(email, perm_type=account_type, role=role)
+
+def list_permissions(sheet):
+	permissions = sheet.list_permissions()
+
+	return permissions
+
+def remove_permissions(sheet,email):
+	sheet.remove_permissions(email)
+
+def select_worksheetByIndex(sheet,index):
+	worksheet = sheet.get_worksheet(index)
+
+	return worksheet	
+
+def select_worksheetByTitle(sheet,title):
+	worksheet = sheet.worksheet(title)
+
+	return worksheet
+
+def new_worksheet(sheet,title,row,column,index=None):
+	sheet.add_worksheet(title=title, rows=row, cols=column,index=index)
+
+def update_vel(worksheet,cell,vel):
+	worksheet.update(cell,vel)
+
+
+
+
+scope = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"]
+
+#Account = createAccount('D:/googlesheet/key/credentials.json',scope)
+
+#sheet = openByURL(Account,'https://docs.google.com/spreadsheets/d/1c8F2L0ndzgv1qzf5on7-J3t0GHLaQ1f3gznfJ6Ad7AQ/edit?usp=sharing')
+
+#new_worksheet(sheet,'test2',10,20)
---
+++ test.py
@@ -0,0 +1,34 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+from pprint import pprint
+import pandas as pd
+
+###pip install --upgrade oauth2client PyOpenSSL gspread####
+
+scope = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('D:/googlesheet/key/credentials.json',scope)
+
+client = gspread.authorize(credentials)
+
+worksheet = client.open('test2')
+
+sheet = worksheet.worksheet('A worksheet')
+
+print (sheet.get('A1:B2',))
+
+#worksheet.add_worksheet(title='title', rows=100, cols=20,index=0)
+
+#worksheet.share('bossdevil62@gmail.com', perm_type='user', role='owner')
+
+#w2 = worksheet.get_worksheet(1)
+
+#cell = w2.find("Boss")
+#val = w2.acell('A1').value
+
+#w2.update('A1:B2', [[1, 2], [3, 4]])
+
+#dataframe = pd.DataFrame(w2.get_all_records())
+#print (dataframe.columns.values.tolist())
+#worksheet.remove_permissions('i.somnark@gmail.com')
+#print ()
