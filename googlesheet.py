6508b9c4cc27f8f330abf5c0b5a093ef15cc5330
Itsada <75968122+Itsadasomnark@users.noreply.github.com>
2020-12-15 15:27:54 +0700

    add test

--- googlesheet.py
+++ googlesheet.py
@@ -63,11 +63,14 @@ def select_worksheetByTitle(sheet,title):
 def new_worksheet(sheet,title,row,column,index=None):
 	sheet.add_worksheet(title=title, rows=row, cols=column,index=index)
 
-def update_val(worksheet,cell,val):
-	worksheet.update(cell,val)
+def update_val(worksheet,acell,val):
+	worksheet.update(acell,val)
 
-def get_val_acell(worksheet,cell):
-	val = worksheet.get(cell)
+def update_val_cell(worksheet,row,column,val):
+	worksheet.update_cell(row, column, val)
+
+def get_val_acell(worksheet,acell):
+	val = worksheet.get(acell)
 
 	return val
 
@@ -82,10 +85,9 @@ def get_all_val_dicts(worksheet):
 	return val_dicts
 
 def get_all_val_list(worksheet):
-	val_list = sheet.get_all_values()
+	val_list = worksheet.get_all_values()
 
 	return val_list
-
 
 def append_row(worksheet,val):
 	worksheet.append_row(val)
@@ -101,3 +103,54 @@ def add_column(worksheet,val):
 def add_row(worksheet,val):
 	worksheet.add_rows(val)
 
+def test():
+	scope = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"]
+
+	keysfile = 'D:/googlesheet/key/credentials.json'
+
+	client = createAccount(keysfile,scope)
+
+	sheet = openByURL(client, 'https://docs.google.com/spreadsheets/d/1c8F2L0ndzgv1qzf5on7-J3t0GHLaQ1f3gznfJ6Ad7AQ/edit?usp=sharing')
+
+	sheet2 = openByTitle(client, 'test')
+
+	sheet3 = createNewSheet(client, 'test3')
+
+	email = 'bossdevil62@gmail.com'
+	shareSheet(sheet3, email, 'user', 'owner')
+
+	premissions = list_permissions(sheet)
+
+	remove_permissions(sheet2, email) #only owner
+
+	ws = select_worksheetByIndex(sheet, 0)
+
+	ws2 = select_worksheetByTitle(sheet, 'A worksheet')
+
+	ws3 = new_worksheet(sheet, 'New Worksheet', 10, 10)
+
+	update_val(ws, 'A1', 'Itsada')
+
+	update_val_cell(ws,1,2,'Somnark')
+
+	val = get_val_acell(ws, 'A1')
+
+	val2 = get_val_cell(ws, 1, 1)
+
+	val3 = get_all_val_dicts(ws)
+
+	val4 = get_all_val_list(ws)
+
+
+	info = [100, 200, 300, 400]
+	append_row(ws, info)
+
+	add_column(ws, 5)
+
+	add_row(ws, 5)
+
+	list_cell = finditem(ws, 'Itsada')
+	row = list_cell[0].row
+	column = list_cell[0].col
+
+test()
